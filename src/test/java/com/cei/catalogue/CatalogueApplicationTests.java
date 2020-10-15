6b4cf65dce2c1d1040e0c921daae250e74d181c4
Gonzalo Abeiro <gonzalo.abeiro@mercadolibre.com>
2020-10-15 00:01:10 -0300

    new proyect CEI - initial commit

---
+++ src/test/java/com/cei/catalogue/CatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cei.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

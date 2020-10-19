203cff03edb23eaa86bfc217d406abcc35978a6f
Gonzalo Abeiro <gonzalo.abeiro@mercadolibre.com>
2020-10-19 11:28:02 -0300

    update Controller

--- src/main/java/com/cei/catalogue/CatalogueApplication.java
+++ src/main/java/com/cei/catalogue/CatalogueApplication.java
@@ -2,6 +2,8 @@ package com.cei.catalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class CatalogueApplication {
@@ -10,4 +12,13 @@ public class CatalogueApplication {
         SpringApplication.run(CatalogueApplication.class, args);
     }
 
+    @GetMapping("/debut")
+    public String getDebutByPlayer(@RequestParam(value = "userId") String userId) {
+        return String.format("Debutaste el día %s!", userId);
+    }
+
+    @GetMapping("/played-matches")
+    public String getPlayedMatchesByUser(@RequestParam(value = "userId") String userId) {
+        return String.format("%s, jugaste %d con el CEI!!", userId);
+    }
 }
